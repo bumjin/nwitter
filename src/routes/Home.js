263438dc616525d129d11190b94ba49fb5b920d1
bumjin <bumjin@gmail.com>
2020-09-27 16:30:28 +0900

    Use firestore Database

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,9 +1,15 @@
+import { dbService } from "fbase";
 import React, { useState } from "react";
 
 const Home = () => {
     const [nweet, setNweet] = useState("")
-    const onSubmit = (event) => {
+    const onSubmit = async (event) => {
         event.preventDefault();
+        dbService.collection("nweets").add({
+            nweet: nweet,
+            createAt: Date.now()
+        })
+        setNweet("")
     }
     const onChange = (event) => {
         const {
