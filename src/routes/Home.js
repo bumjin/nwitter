2392cedd9d47098cb01435d54c2a52a54d253e0e
bumjin <bumjin@gmail.com>
2020-09-28 17:54:38 +0900

    Preview Image part One

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -4,7 +4,16 @@ import Nweet from "components/Nweet"
 const Home = ({ userObj }) => {
     const [nweet, setNweet] = useState("")
     const [nweets, setNweets] = useState([])
-
+    const onFileChange = (event) => {
+        const { target: { files } } = event;
+        const theFile = files[0];
+        //console.log(file)
+        const reader = new FileReader();
+        reader.onloadend = (finishedEvent) => {
+            console.log(finishedEvent)
+        }
+        reader.readAsDataURL(theFile);
+    }
     useEffect(() => {
         dbService.collection("nweets").onSnapshot(snapshot => {
             //console.log(snapshot.docs);
@@ -35,6 +44,7 @@ const Home = ({ userObj }) => {
         <div>
             <form onSubmit={onSubmit}>
                 <input value={nweet} onChange={onChange} type="text" placeholder="What's on your mind?" maxLength={120} />
+                <input type="file" accept="image/*" onChange={onFileChange}></input>
                 <input type="submit" value="Nweet" />
             </form>
             <div>
